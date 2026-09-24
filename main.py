53420fb3105502bce3dc42844747e4cdb1bf2b45
nitinyadav798906 <nitinyadav798906@gmail.com>
2026-09-24 19:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -105,11 +105,6 @@ def generate_html(file_name, content, is_protected=False):
        # --- S3 Amazon Link Interception ---
         if "video-streaming-source.s3.ap-south-1.amazonaws.com" in url:
             url = f"https://sujaladda.netlify.app/stream.m3u8?url={url}"
-            
-            elif "/master.mpd" in url:
-    vid_id = url.split("/")[-2]
-    new_url = f"https://pw-study-bison.space-z.ai/api/hls/{vid_id}/master.m3u8"
-    videos.append((name, new_url))
        
       if any(x in low_u for x in [".m3u8", ".mpd", ".mp4", ".mkv"]): 
             t = "VIDEO"; v_c += 1; icon = "🎥"
